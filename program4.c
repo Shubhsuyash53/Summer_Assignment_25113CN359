a47f577ad003b820133e3505ba8cde87302ddc02
Suyash Saini <sainisuyash53@gmail.com>
2026-05-28 19:50:29 +0530

    program 4 uploaded

---
+++ program4.c
@@ -0,0 +1,15 @@
+//Program to count digits of a number
+#include<stdio.h>
+int main()
+{
+    int r=0,n,i;
+    printf ("Enter a number");
+    scanf ("%d",&n);
+    while (n>0)
+    {
+        r=r+1;
+        n=n/10;
+    }
+    printf ("Number of digits is %d",r);
+    return 0;
+}
