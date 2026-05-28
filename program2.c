672a7107218f28ed359384eb6d9e9de6dee20891
Suyash Saini <sainisuyash53@gmail.com>
2026-05-28 19:32:22 +0530

    uploaded program2 and program 3

---
+++ program2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,p=1;
+    printf ("Enter the number");
+    scanf ("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        p=i*n;
+        printf ("\n");
+        printf ("%d",p);
+    }
+    return 0;
+}
---
+++ program3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,p=1;
+    printf ("Enter the number to find factorial");
+    scanf ("%d",&n);
+    for (i=n;i>=1;i--)
+    {
+        p=p*i;
+    }
+    printf ("The factorial is %d",p);
+    return 0;
+}
