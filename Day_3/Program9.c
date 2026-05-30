1988a8a9d0f7960f5dae4df4d678e256e8f352cf
Suyash Saini <sainisuyash53@gmail.com>
2026-05-30 17:17:53 +0530

    Checking prime no.

---
+++ Day_3/Program9.c
@@ -0,0 +1,25 @@
+//Program to check if a number is prime or not
+#include<stdio.h>
+int main()
+{
+    int n,i,prime=0;
+    printf ("Enter a number");
+    scanf ("%d",&n);
+    for (i=2;i<=n/2;i++)
+    {
+        if (n%i==0)
+        {
+            prime=1;
+            break;
+        }
+    }
+    if (prime==0)
+    {
+        printf ("%d is a prime number",n);
+    }
+    else
+    {
+        printf ("%d is not a prime number",n);
+    }
+    return 0;
+}
