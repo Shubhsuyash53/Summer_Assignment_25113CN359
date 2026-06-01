8bbb2fd14e93d06f335b51616d4c5f6eb98f1372
Suyash Saini <sainisuyash53@gmail.com>
2026-06-01 13:37:15 +0530

    Recursive reverse

---
+++ Day_7/Program28.c
@@ -0,0 +1,18 @@
+//Program to recursive reverse number
+#include<stdio.h>
+int reverse(int n, int rev)
+{
+    if(n==0)
+        return rev;
+    else
+        return reverse(n/10, rev*10 + n%10);
+}
+int main()
+{
+    int n, result;
+    printf("Enter a number:");
+    scanf("%d", &n);
+    result = reverse(n, 0);
+    printf("Reverse of %d is: %d", n, result);
+    return 0;
+}
