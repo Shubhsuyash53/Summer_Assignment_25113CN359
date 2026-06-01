5a223e0b252837fab471984729cebacef8748922
Suyash Saini <sainisuyash53@gmail.com>
2026-06-01 12:24:54 +0530

    Fibonacci Series

---
+++ Day_4/Program13.c
@@ -0,0 +1,22 @@
+//Program to generate fibonacci series
+#include<stdio.h>
+int main ()
+{
+    int i, n, a=0, b=1, next;
+    printf ("Enter number of terms:");
+    scanf ("%d", &n);
+    printf ("Fibonacci Series:\n");
+    for (i=0;i<n;i++)
+    {
+        if (i<=1)
+        next=i;
+        else
+        {
+            next = a + b;
+            a = b;
+            b = next;
+        }
+        printf ("%d ", next);
+    }
+    return 0;
+}
