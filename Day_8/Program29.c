f2646c7cd1a73e00b3f070276f125b012fd70031
Suyash Saini <sainisuyash53@gmail.com>
2026-06-09 13:25:22 +0530

    pyramid

---
+++ Day_8/Program29.c
@@ -0,0 +1,15 @@
+//Program to print half pyramid pattern
+#include<stdio.h>
+int main()
+{
+    int j,i;
+    for (i=1; i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf ("\n");
+    }
+return 0;
+}
