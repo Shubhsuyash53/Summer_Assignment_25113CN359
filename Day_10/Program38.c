818ef2b8b322f8dddcd7aa127664944640714d2e
Suyash Saini <sainisuyash53@gmail.com>
2026-06-11 22:19:37 +0530

    Inverted pyramid of stars

---
+++ Day_10/Program38.c
@@ -0,0 +1,17 @@
+//program to print Inverted pyrami of stars
+#include <stdio.h>
+
+int main() {
+    int i,j,k;
+    for (i=5; i >= 1; i--) {
+        for ( j = 1; j <= 5- i; j++) {
+            printf(" ");
+        }
+        for (int k = 1; k <= (2 * i - 1); k++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
