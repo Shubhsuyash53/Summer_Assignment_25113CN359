6c8cfc7fe6247cfe92f77c98023ada8b22a3b0a0
Suyash Saini <sainisuyash53@gmail.com>
2026-06-16 12:49:36 +0530

    reversing array

---
+++ Day_15/Program57.c
@@ -0,0 +1,25 @@
+//Program to reverse an array
+#include<stdio.h>
+int main()
+{
+    int n,i,a[20],temp;
+    printf ("Enter the number of elements");
+    scanf ("%d",&n);
+    printf ("Enter the elements");
+    for(i=0;i<n;i++)
+    {
+        scanf ("%d",&a[i]);
+    }
+    for(i=0;i<n/2;i++)
+    {
+        temp=a[i];
+        a[i]=a[n-1-i];
+        a[n-1-i]=temp;
+    }
+    printf ("Reversed array is:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
