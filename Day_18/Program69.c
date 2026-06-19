0348a24c66323b3aef86943f6e2c7c59bd69ce29
Suyash Saini <sainisuyash53@gmail.com>
2026-06-19 11:54:52 +0530

    Bubble Sort

--- Day_18/Program69.c
+++ Day_18/Program69.c
@@ -1,48 +1,31 @@
-//Program to add two matrices
+//Program to bubble sort an array
 #include<stdio.h>
 int main()
 {
-    int r1,c1,r2,c2,i,j,a[20][20],b[20][20],c[20][20];
-    printf ("Enter the number of rows and columns of first matrix:");
-    scanf ("%d%d",&r1,&c1);
-    printf ("Enter the elements of first matrix:");
-    for(i=0;i<r1;i++)
+    int n,i,j,a[20],temp;
+    printf ("Enter the number of elements:");
+    scanf ("%d",&n);
+    printf ("Enter the elements:");
+    for(i=0;i<n;i++)
     {
-        for(j=0;j<c1;j++)
+        scanf ("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=0;j<n-i-1;j++)
         {
-            scanf ("%d",&a[i][j]);
+            if(a[j]>a[j+1])
+            {
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
         }
     }
-    printf ("Enter the number of rows and columns of second matrix:");
-    scanf ("%d%d",&r2,&c2);
-    if(r1!=r2 || c1!=c2)
+    printf ("Sorted array is:");
+    for(i=0;i<n;i++)
     {
-        printf ("Addition is not possible");
-        return 0;
-    }
-    printf ("Enter the elements of second matrix:");
-    for(i=0;i<r2;i++)
-    {
-        for(j=0;j<c2;j++)
-        {
-            scanf ("%d",&b[i][j]);
-        }
-    }
-    for(i=0;i<r1;i++)
-    {
-        for(j=0;j<c1;j++)
-        {
-            c[i][j]=a[i][j]+b[i][j];
-        }
-    }
-    printf ("Sum of two matrices is:");
-    for(i=0;i<r1;i++)
-    {
-        for(j=0;j<c1;j++)
-        {
-            printf ("%d ",c[i][j]);
-        }
-        printf ("\n");
+        printf("%d ",a[i]);
     }
     return 0;
 }
