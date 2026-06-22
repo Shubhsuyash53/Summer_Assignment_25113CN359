62a1b234652336f4e5ea861f4c8607b38a800111
Suyash Saini <sainisuyash53@gmail.com>
2026-06-22 20:31:28 +0530

    number of words

---
+++ Day_22/Program86.c
@@ -0,0 +1,16 @@
+//Program to count words in a sentence
+#include<stdio.h>
+int main(){
+    char str[100];
+    int i=0,words=1;
+    printf("Enter a sentence: ");
+    gets(str);
+    while(str[i]!='\0'){
+        if(str[i]==' '){
+            words++;
+        }
+        i++;
+    }
+    printf("Number of words in the sentence: %d",words);
+    return 0;
+}
